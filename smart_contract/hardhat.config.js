c5b4a991a5831423919b21d3a61d2467aa2aa637
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-08-11 16:24:51 +0100

    create smart contract and deploy file

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      accounts: ['b37af6ba2d50deb9077c7b15954373c7897420c45d601d97889fecc163a26462'],
+    },
+  },
+};
